47b9af45e8f4c1f5a2e1a44ef8e53651779ec730
Md Jeshan Khan <jeshank1123@gmail.com>
2025-03-30 20:57:50 +0600

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/hero.jsx'
+import Service from './components/service.jsx'
 
 function App() {
  
@@ -9,6 +10,7 @@ function App() {
     <>
   <Navbar/>
   <Hero/>
+  <Service/>
 
     </>
   )
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,22 +1,24 @@
 import React from "react";
+import img1 from '../Image/ce203ad8-16ee-4b33-87d4-bb417785b1a5-1.png'
 
 const hero = () => {
   return (
     <>
       <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero-content flex-col lg:flex-row-reverse gap-50">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            className="max-w-sm rounded-lg shadow-2xl"
+            src={img1}
+            className="rounded-lg shadow-2xl md:w-[600px] md:h-[600px]" 
           />
           <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+            <h1 className="text-5xl font-bold">Md.Jeshan Khan</h1>
+            <h1 className="text-2xl py-5 font-bold text-[#179800]">Full Stack Developer </h1>
+            <p className="py-3">
+              Hello!My name is Jeshan.I'm Professional Full StackWeb
+              Developer.I can do Back End and Front-End work for your Commercial or Business
+              Web Side.That will be helpful for your business.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <button className="btn btn-primary">Contract</button>
           </div>
         </div>
       </div>
